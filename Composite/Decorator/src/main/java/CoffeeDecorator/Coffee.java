575c889d983748c855e3be7a128a71935b0db9a0
RaduRaresGeorge <rradu@myuax.com>
2024-04-16 13:13:09 +0200

    duplicate delete

--- Composite/Decorator/src/main/java/CoffeeDecorator/Coffee.java
+++
@@ -1,6 +0,0 @@
-package CoffeeDecorator;
-
-public abstract class Coffee {
-    public abstract double getCost(); // Returns the cost of the coffee
-    public abstract String getIngredients(); // Returns the ingredients of the coffee
-}
--- Composite/Decorator/src/main/java/DataDecorator/DataSource.java
+++
@@ -1,7 +0,0 @@
-package DataDecorator;
-
-public interface DataSource {
-    void writeData(String data);
-
-    String readData();
-}
