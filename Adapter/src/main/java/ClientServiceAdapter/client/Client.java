ea259866975681b420e0257c7c82ac4845db11bc
RaduRaresGeorge <rradu@myuax.com>
2024-04-18 08:39:47 +0200

    doc

--- Adapter/src/main/java/ClientServiceAdapter/client/Client.java
+++ Adapter/src/main/java/ClientServiceAdapter/client/Client.java
@@ -1,6 +1,15 @@
 package src.main.java.ClientServiceAdapter.client;
 
+/**
+ * This class represents a client in the Client-Service Adapter pattern.
+ * It provides a method to execute a task.
+ */
 public class Client {
+
+    /**
+     * This method is used to execute a task.
+     * In this context, it simply prints a message to the console.
+     */
     public void execute() {
         System.out.println("Executing client...");
     }
