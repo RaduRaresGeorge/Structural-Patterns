282283fcd5ae885c9c91a3d0107e1477749f9b95
RaduRaresGeorge <rradu@myuax.com>
2024-04-18 08:40:47 +0200

    doc

--- Adapter/src/main/java/ClientServiceAdapter/Demo.java
+++ Adapter/src/main/java/ClientServiceAdapter/Demo.java
@@ -4,12 +4,28 @@ import src.main.java.ClientServiceAdapter.client.Client;
 import src.main.java.ClientServiceAdapter.service.Service;
 import src.main.java.ClientServiceAdapter.adapter.ClientServiceAdapter;
 
+/**
+ * This class represents a demonstration of the Client-Service Adapter pattern.
+ * It creates a service, wraps it in a ClientServiceAdapter (which is a type of Client),
+ * and then executes the client.
+ */
 public class Demo {
+    /**
+     * The main method of the Demo class.
+     * It creates a Service, wraps it in a ClientServiceAdapter, and then executes the client.
+     *
+     * @param args command line arguments. This program does not use command line arguments.
+     */
     public static void main(String[] args) {
+        // Create a new Service
         Service service = new Service();
+
+        // Create a new ClientServiceAdapter, passing the service to its constructor
+        // The ClientServiceAdapter is a type of Client, so we can store it in a Client variable
         Client client = new ClientServiceAdapter(service);
+
+        // Execute the client
         client.execute();
     }
 }
-
 
--- Adapter/src/main/java/ClientServiceAdapter/service/Service.java
+++ Adapter/src/main/java/ClientServiceAdapter/service/Service.java
@@ -1,6 +1,15 @@
 package src.main.java.ClientServiceAdapter.service;
 
+/**
+ * This class represents a service in the Client-Service Adapter pattern.
+ * It provides a method to execute a service.
+ */
 public class Service {
+
+    /**
+     * This method is used to execute a service.
+     * In this context, it simply prints a message to the console.
+     */
     public void executeService() {
         System.out.println("Executing service...");
     }
